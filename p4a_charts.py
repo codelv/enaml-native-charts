ca28124dc2cc6104341d3705b0f9bdc735c2996a
frmdstryr <frmdstryr@gmail.com>
2017-10-29 10:40:08 -0400

    Update recipe

--- p4a_charts.py
+++ p4a_charts.py
@@ -12,7 +12,7 @@ class EnamlNativeRecipe(PythonRecipe):
     def download(self):
         """ Copy it right from the source """
         #: Zip the srz
-        src_root = abspath(join(self.ctx.root_dir, '..', '..'))
+        src_root = abspath(join(self.ctx.root_dir, '..', '..', 'venv', 'packages', self.name))
         with current_directory(src_root):
             sh.zip('-r', join(self.ctx.packages_path, self.name, self.url), 'src')
 
