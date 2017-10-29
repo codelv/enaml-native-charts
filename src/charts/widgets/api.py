69b52a87ded93c1f1e48436ddd2c98e026b77ecb
frmdstryr <frmdstryr@gmail.com>
2017-10-29 15:00:36 -0400

    Add api

---
+++ src/charts/widgets/api.py
@@ -0,0 +1,22 @@
+'''
+Copyright (c) 2017, Jairus Martin.
+
+Distributed under the terms of the MIT License.
+
+The full license is in the file COPYING.txt, distributed with this software.
+
+Created on Oct 23, 2017
+
+@author: jrm
+'''
+from .chart_view import DataSet, LineChart, ScatterChart, BarChart, PieChart
+
+
+def install():
+    from enamlnative.widgets import api
+
+    setattr(api, 'DataSet', DataSet)
+    setattr(api, 'LineChart', LineChart)
+    setattr(api, 'ScatterChart', ScatterChart)
+    setattr(api, 'BarChart', BarChart)
+    setattr(api, 'PieChart', PieChart)
